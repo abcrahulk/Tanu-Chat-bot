27b9393de2e14c390f5e84f7c24b654311f1b62b
Krishna jha <129253790+Krishnauff@users.noreply.github.com>
2023-03-31 16:08:30 +0530

    Update inline.py

--- inline.py
+++ inline.py
@@ -18,7 +18,7 @@ DEV_OP = [
         InlineKeyboardButton(text="🚀 ʜᴇʟᴘ & ᴄᴍᴅs 🚀", callback_data="HELP"),
     ],
     [
-        InlineKeyboardButton(text="❄️ sᴏᴜʀᴄᴇ ❄️", callback_data="https://github.com/Krishnauff/krishna-aaruchatbot"),
+        InlineKeyboardButton(text="❄️ sᴏᴜʀᴄᴇ ❄️", callback_data="SOURCE"),
         InlineKeyboardButton(text="☁️ ᴀʙᴏᴜᴛ ☁️", callback_data="ABOUT"),
     ],
 ]
